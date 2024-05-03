b7165a83f95d349bf833cc96185662baff765905
UDAYKRISHNA999 <152253324+UDAYKRISHNA999@users.noreply.github.com>
2024-05-03 05:12:12 +0000

    updated repo with variables file where it has some code and info about variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 120120;
+let accountEmailId = "udaykrishna@gmail.com";
+var accountPassword = "1451451";
+accountCity = "Hyderabad";
+
+
+// console.log(accountId, accountEmailId);
+
+/*
+Prefer not to use var keyword for variable declaration 
+as it can cause issues while assigning a new value to variable that is declared with var key word .
+So, mostly prefer to use const or let keywords..
+*/
+
+
+
+console.table([accountId, accountEmailId, accountPassword, accountCity])
