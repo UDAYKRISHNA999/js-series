0b8836864b220ed46f11f8e628b46b482e80ee13
UDAYKRISHNA999 <152253324+UDAYKRISHNA999@users.noreply.github.com>
2024-05-09 12:21:07 +0000

    added four.js file which consists about forIn loop which is generally performed on objects

---
+++ 05_iterations/four.js
@@ -0,0 +1,33 @@
+/*
+In between forin and forof :
+We use forof -- objects
+We use forin -- Array(not only for in but in between these two)
+*/
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+//We can't use forin for Maps as it return nothing if we perform forin on Maps (refer above example)
