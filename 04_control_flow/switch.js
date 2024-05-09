7ad082c910f3266ea88f4227af7e29af261765d7
UDAYKRISHNA999 <152253324+UDAYKRISHNA999@users.noreply.github.com>
2024-05-09 06:09:18 +0000

    created a new folder 04 control flow which consists of conditiona; statements and etc with detailed notes

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+/* 
+In switch case every case executes excluding default statement. 
+
+case is also case sensitive
+
+
+*/ 
